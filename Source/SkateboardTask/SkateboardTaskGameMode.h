2ee6ab6d936cb090506413d49e29537dcaf30ab5
nstoilov99 <nstoilov99@gmail.com>
2025-03-11 15:14:26 +0200

    Initial commit

---
+++ Source/SkateboardTask/SkateboardTaskGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SkateboardTaskGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASkateboardTaskGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASkateboardTaskGameMode();
+};
+
+
+
